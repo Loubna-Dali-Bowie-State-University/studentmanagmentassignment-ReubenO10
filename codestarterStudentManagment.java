1fe0a8919f8bc60f8762e7c89e09898a916824ac
ReubenO10 <ODEDAIROR1031@students.bowiestate.edu>
2024-11-12 21:06:39 -0500

    Update codestarterStudentManagment.java

--- codestarterStudentManagment.java
+++ codestarterStudentManagment.java
@@ -1,39 +1,103 @@
-public class Student {
+public class Main {
+
+    public static void main(String[] args) {
+        StudentManagement sm = new StudentManagement();
+
+        Student s1 = new Student("Messi", 1, new int[]{100, 95, 100});
+        Student s2 = new Student("Hazard", 2, new int[]{95, 100, 95, 95});
+        Student s3 = new Student("Neymar", 3, new int[]{95, 100, 92});
+
+        sm.addStudent(s1);
+        sm.addStudent(s2);
+        sm.addStudent(s3);
+
+        sm.displayAllStudents();
+    }
+}
+
+class Student {
     private String name;
     private int id;
     private int[] grades;
-
-    // Default Constructor
-   
+    
+    public Student() {
+        this.name = "Unknown";
+        this.id = 0;
+        this.grades = new int[0];
     }
 
-    // Parameterized Constructor
     public Student(String name, int id, int[] grades) {
         this.name = name;
         this.id = id;
-        setGrades(grades); // using setter to apply validation
+        setGrades(grades); 
     }
 
-    // Getters
-  
+    public String getName() {
+        return name;
+    }
 
-    // Setters
-    
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    // Method to calculate average grade
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int[] getGrades() {
+        return grades;
+    }
+
+    public void setGrades(int[] grades) {
+        for (int grade : grades) {
+            if (grade < 0 || grade > 100) {
+                System.out.println("Invalid grade: " + grade + ". Grades must be between 0 and 100.");
+                return;
+            }
+        }
+        this.grades = grades;
+    }
+
+    public double calculateAverage() {
+        if (grades.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return (double) sum / grades.length;
+    }
 }
-public class StudentManagement {
+
+class StudentManagement {
     private Student[] students;
     private int studentCount;
 
-    // Constructor to initialize the students array
+    public StudentManagement() {
+        students = new Student[5];
+        studentCount = 0;
+    }
 
+    public void addStudent(Student student) {
+        if (studentCount < students.length) {
+            students[studentCount] = student;
+            studentCount++;
+        } else {
+            System.out.println("No more students can be added.");
+        }
+    }
 
-    // Method to add a student to the students array
- 
-
-    // Method to display all students
-   
-   
-           
+    public void displayAllStudents() {
+        for (int i = 0; i < studentCount; i++) {
+            Student student = students[i];
+            System.out.println("Name: " + student.getName() +
+                               ", ID: " + student.getId() +
+                               ", Average Grade: " + student.calculateAverage());
+        }
+    }
 }
